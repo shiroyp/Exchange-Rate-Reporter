16ccf2019faddeae5d48107a62a7d4b497b18a6f
shiroyp <shiroyp@users.noreply.github.com>
2016-06-24 16:02:28 +0100

    Create exchange_rate_reporter_cloudwatch.py

---
+++ exchange_rate_reporter_cloudwatch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+__author__ = 'shiroyp'
+from urllib.request import urlopen
+from boto.ec2.cloudwatch import CloudWatchConnection
+from argparse import ArgumentParser
+import csv
+import io
+
+parser = ArgumentParser(description='Converts the currency as per current exchange rate')
+parser.add_argument('-f', '--from', default='EUR')
+parser.add_argument('-t', '--to', default='INR')
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+args = (vars(parser.parse_args()))
+from_currency = args['from']
+to_currency = args['to']
+
+URL = 'http://download.finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=' + from_currency + to_currency +'=X'
+webpage = urlopen(URL)
+datareader = csv.reader(io.TextIOWrapper(webpage))
+# readdata = webpage.read()
+# csvdata = csv.reader(readdata.decode())
+for row in datareader:
+    #print ('1 %s = %s %s' % (from_currency, row[1], to_currency))
+    rate = float(row[1])
+
+
+cwc = CloudWatchConnection()
+cwc.put_metric_data(namespace="ExchangeRate",name=from_currency+"-to-"+to_currency,unit="None",value=rate)
